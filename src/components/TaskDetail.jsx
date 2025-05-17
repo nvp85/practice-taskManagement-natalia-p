fbc8b7ba7a583786d8e753721b3ca15aefa72001
nvp85 <nvp@zhukov.al>
2025-05-16 20:55:23 -0500

    add conditional rendering in case if no task was found

--- src/components/TaskDetail.jsx
+++ src/components/TaskDetail.jsx
@@ -5,11 +5,16 @@ const TaskDetail = () => {
     const { taskId } = useParams();
     const task = tasks.find(task => task.id == taskId);
     return (
-        <div id="task-container">
-            <h3>{task.title}</h3>
-            <p>{task.description}</p>
-            <Link to="/tasks">Back to the task list</Link>
-        </div>
+        <>
+        {task 
+            ? (<div id="task-container">
+                <h3>{task.title}</h3>
+                <p>{task.description}</p>
+                <Link to="/tasks">Back to the task list</Link>
+                </div>) 
+            : (<p>No task was found</p>)
+        }
+        </>
     )
 }
 export default TaskDetail;
